ca1846b0e1bc8dc5c0e471438bb3a2b0e9f14b76
inne <mrbelongtim@gmail.com>
2024-11-12 15:37:49 +0100

    finished task -1

---
+++ task-1.js
@@ -0,0 +1,19 @@
+function executeComplexOperation() {
+    const randomDelay = Math.floor(Math.random() * (500 - 100 + 1)) + 100;
+    return new Promise(resolve => setTimeout(resolve, randomDelay));
+  }
+  
+  async function executeAndInform(id) {
+    await executeComplexOperation();
+    console.log(`Operation ${id} complete`);
+  }
+  
+  async function executeAll(array) {
+    for (const id of array) {
+      await executeAndInform(id);
+    }
+    console.log('All operations completed!');
+  }
+  
+  executeAll([1, 2, 3, 4, 5]);
+  
